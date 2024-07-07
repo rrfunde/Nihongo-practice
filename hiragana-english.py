e75b6d2a1fcebb5bc68e08b6dd95cb28bb527c60
Rohit Funde <rrfunde@gmail.com>
2024-07-07 14:51:47 +0900

    updated

--- hiragana-english.py
+++ hiragana-english.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import MySQLdb
+import mysql.connector
 from MySQLConnectionHandler import MySQLConnectionHandler
 
 hiragana = ""
@@ -14,24 +14,24 @@ cursor = mysqlConnector.getCursor()
 
 # Parse data from file
 filename = "data files/hiragana-english.txt"
-file = open(filename, "r")
-for line in file:
-    hiragana, english = line.split("	", 1)
-    print line
-    # sql = "INSERT INTO vocabulary(word, \
-    #        hiragana, category) \
-    #        VALUES ('%s', '%s', '%s')" % \
-    #        (english, hiragana, category)
-    # try:
-    #    # Execute the SQL command
-    #    x = cursor.execute(sql)
-    #    # Commit your changes in the database
-    #    db.commit()
-    #    print "success"
-    # except:
-    #    # Rollback in case there is any error
-    #    db.rollback()
-    #    print "failed"
+with open(filename, "r", encoding="utf-8") as file:
+    for line in file:
+        hiragana, english = line.split("\t", 1)
+        print(line.strip())
+
+        sql = "INSERT INTO vocabulary (word, hiragana, category) VALUES (%s, %s, %s)"
+        values = (english.strip(), hiragana.strip(), category)
+
+        try:
+            # Execute the SQL command
+            cursor.execute(sql, values)
+            # Commit your changes in the database
+            db.commit()
+            print("success")
+        except Exception as e:
+            # Rollback in case there is any error
+            db.rollback()
+            print("failed:", e)
 
 # disconnect from server
 db.close()
--- image-Downloader.py
+++ image-Downloader.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-import urllib2
+import urllib.request
 import json
 from MySQLConnectionHandler import MySQLConnectionHandler
 
@@ -7,46 +7,48 @@ mysqlConnector = MySQLConnectionHandler()
 db = mysqlConnector.getDatabase()
 cursor = mysqlConnector.getCursor()
 
-header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
+header = {
+    'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"
+}
 sql = "SELECT hiragana, word FROM vocabulary"
 
-def get_soup(url,header):
-    return BeautifulSoup(urllib2.urlopen(urllib2.Request(url,headers=header)),'html.parser')
+def get_soup(url, header):
+    req = urllib.request.Request(url, headers=header)
+    response = urllib.request.urlopen(req)
+    return BeautifulSoup(response, 'html.parser')
 
 try:
-   # Execute the SQL command
-   cursor.execute(sql)
-   # Fetch all the rows in a list of lists.
-   results = cursor.fetchall()
-   for row in results:
-      hiragana = row[0]
-      word = row[1] + " animated"
-      query = word
-      query= query.split()
-      query='+'.join(query)
-      url="https://www.google.co.in/search?q="+query+"&source=lnms&tbm=isch"
+    # Execute the SQL command
+    cursor.execute(sql)
+    # Fetch all the rows in a list of lists.
+    results = cursor.fetchall()
+    for row in results:
+        hiragana = row[0]
+        word = row[1] + " animated"
+        query = word.split()
+        query = '+'.join(query)
+        url = "https://www.google.co.in/search?q=" + query + "&source=lnms&tbm=isch"
 
-      soup = get_soup(url,header)
-      i = 0
-      linkUrl = ""
-      for a in soup.find_all("div",{"class":"rg_meta"}):
-             link , Type =json.loads(a.text)["ou"]  ,json.loads(a.text)["ity"]
-             linkUrl = linkUrl + " " + link
-             i += 1
-             if i > 3:
-                 break
+        soup = get_soup(url, header)
+        i = 0
+        linkUrl = ""
+        for a in soup.find_all("div", {"class": "rg_meta"}):
+            link, Type = json.loads(a.text)["ou"], json.loads(a.text)["ity"]
+            linkUrl = linkUrl + " " + link
+            i += 1
+            if i > 3:
+                break
 
-
-      sql1 = "UPDATE vocabulary SET imageUrl = (%s) WHERE hiragana = (%s)"
-      try:
-           cursor.execute(sql1, (linkUrl,hiragana))
-           db.commit()
-           print "success"
-      except:
-           db.rollback()
-           print "failed"
-except:
-   print "Error: unable to fecth data"
+        sql1 = "UPDATE vocabulary SET imageUrl = %s WHERE hiragana = %s"
+        try:
+            cursor.execute(sql1, (linkUrl.strip(), hiragana))
+            db.commit()
+            print("success")
+        except Exception as e:
+            db.rollback()
+            print("failed:", e)
+except Exception as e:
+    print("Error: unable to fetch data:", e)
 
 # disconnect from server
 db.close()
